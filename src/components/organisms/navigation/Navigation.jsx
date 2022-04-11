cdf6e9a843da2f4054c6f7dffc8697de010e19fe
Maria Paola Valencia <mapava1987@hotmail.com>
2022-04-11 11:05:53 -0500

    Maquetar el brand de la navegación

--- src/components/organisms/navigation/Navigation.jsx
+++ src/components/organisms/navigation/Navigation.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import logoApp from "assets/img/logo.png"
 
 const Navigation = () => {
-    return <nav></nav>;
+    return (
+        <nav className="o-navigation">
+            <div ClassName="o-navigation_brand">
+                <img 
+                className="o-navigation__brand__image"
+                src={logoApp} 
+                alt="logo de la aplicación" 
+                />
+                <span>POKEDEX_</span>
+            </div>
+        </nav>
+    );
 };
 
 export default Navigation;
